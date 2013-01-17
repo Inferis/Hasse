78015db6b1332533dd65e6ff5cb64f033cb0ef3d
Tom Adriaenssen <tom.adriaenssen@interfaceimplementation.be>
2013-01-17 22:58:36 +0100

    Initial checkin.

---
+++ Source/Hasse.Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Hasse.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Source/Hasse.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hasse.Web.MvcApplication" Language="C#" %>
