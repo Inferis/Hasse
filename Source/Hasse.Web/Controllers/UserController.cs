99b38e58b5a0c935a9be335961181d6b344aa05b
Tom Adriaenssen <tom.adriaenssen@interfaceimplementation.be>
2013-01-21 20:42:58 +0100

    Working FB auth.

--- Source/Hasse.Web/Controllers/UserController.cs
+++ Source/Hasse.Web/Controllers/UserController.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OAuth2;
+using Hasse.Web.Models;
 
 namespace Hasse.Web.Controllers
 {
     public class UserController : Controller
     {
+        public static readonly string[] ScopeNeeded = { "email", "publish_stream" };
+
+        private WebServerClient FacebookClient()
+        {
+            var descr = new AuthorizationServerDescription {
+                TokenEndpoint = new Uri("https://graph.facebook.com/oauth/access_token"),
+                AuthorizationEndpoint = new Uri("https://graph.facebook.com/oauth/authorize")
+            };
+
+            var client = new WebServerClient(descr) {
+                ClientIdentifier = "335994424835",
+                ClientCredentialApplicator = ClientCredentialApplicator.PostParameter("01832afd7614f05840b908322377fe63"),
+            };
+
+            return client;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var client = FacebookClient();
+            var uri = new Uri(Request.Url.Scheme + "://" + Request.Url.Host + Url.Action("OAuthed", new { id = "facebook" }));
+            return client.PrepareRequestUserAuthorization(ScopeNeeded, uri).AsActionResult();
         }
 
 
+
+        public ActionResult OAuthed(string id)
+        {
+            if (id == "facebook") {
+                var client = FacebookClient();
+                var state = client.ProcessUserAuthorization();
+                if (state != null) {
+                    var graph = new Facebook.FacebookClient(state.AccessToken);
+                    var me = graph.Get<dynamic>("me");
+                    return View(new AuthModel { AccessToken = state.AccessToken, Name = me.name, Email = me.email });
+                }
+            }
+
+            return View();
+        }
     }
+
+
 }
---
+++ Source/Hasse.Web/Models/AuthModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hasse.Web.Models
+{
+    public class AuthModel
+    {
+        public string AccessToken { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
